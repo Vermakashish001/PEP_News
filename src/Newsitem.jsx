a02b0e1218c553100edf830544cdec51877431fc
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-01-24 11:28:39 +0530

    Initial commit

---
+++ src/Newsitem.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./App.css"
+export default function Newsitem({title,description,img,url}) {
+  return (
+    <div>
+        <div className='card'>
+            <h1 className='heading'>{title?title.slice(0,20):"Yahan Title nhi hain"}</h1>
+            <div className="inner">
+            <p className='para'>{description?description.slice(0,80):"Yahan Description Nhi Hain"}</p>
+            <img className="img" src={img?img:"https://img.etimg.com/thumb/msid-106949123,width-1200,height-630,imgsize-43518,overlay-economictimes/photo.jpg"} alt={title}/>
+            </div>
+            <a className='btn' href={url} target='_blank' rel="noreferrer">Read More</a>
+        </div>
+    </div>
+  )
+}
